d50b5ee18bf2eabf3bc5c6a649e9bac5dfd0b876
Félix Gaschi <flx.gsch@gmail.com>
2022-09-21 13:18:57 +0200

    feat: add other realignment strategies to new training loop

--- multilingual_eval/training/epoch_loop.py
+++ multilingual_eval/training/epoch_loop.py
@@ -1,3 +1,4 @@
+import itertools
 import torch
 import logging
 import math
@@ -15,48 +16,75 @@ def get_realignment_loss_for_batch(model, realignment_batch):
     return model(**realignment_batch, return_dict=True)
 
 
-def during_strategy_epoch_loop(
+def epoch_loop(
     model,
     optimizer,
-    task_dataloader,
-    realignment_dataloader,
+    task_dataloader=None,
+    realignment_dataloader=None,
     task_accumulation_steps=1,
     logging_steps=100,
     log_in_wandb=False,
+    nb_iter=None,
 ):
+    if realignment_dataloader is None and task_dataloader is None:
+        raise Exception(
+            "Both task_dataloader and realignment_dataloader cannot be None, we need to train on at least one dataloader"
+        )
+
+    if task_dataloader is None and nb_iter is None:
+        raise Exception(
+            f"If task_dataloader is not provided (got {task_dataloader}), you should provide nb_iter (got {nb_iter})"
+        )
+
+    if nb_iter is not None and task_dataloader is not None:
+        logging.warning(
+            f"nb_iter was provided ({nb_iter}) but so was task_dataloader. nb_iter will be ignored."
+        )
+
+    if task_dataloader is not None:
+        nb_iter = len(task_dataloader)
+
     model.train()
     if log_in_wandb:
         import wandb
 
-    realignment_iterator = iter(realignment_dataloader)
+    if realignment_dataloader is not None:
+        realignment_iterator = iter(realignment_dataloader)
 
-    nb_batch = math.ceil(len(task_dataloader) / task_accumulation_steps)
+    nb_batch = math.ceil(nb_iter / task_accumulation_steps)
 
-    for i, batch in enumerate(task_dataloader):
+    for i, batch in (
+        enumerate(task_dataloader)
+        if task_dataloader is not None
+        else enumerate(itertools.repeat(None, nb_iter))
+    ):
         if i % task_accumulation_steps == 0:
             optimizer.zero_grad()
             accumulated_steps = 0
             total_loss = 0
             task_loss = 0
+            realignment_loss = 0
 
-            realignment_iterator, realignment_batch = get_next_or_restart(
-                realignment_dataloader, realignment_iterator
-            )
+            if realignment_dataloader is not None:
+                realignment_iterator, realignment_batch = get_next_or_restart(
+                    realignment_dataloader, realignment_iterator
+                )
 
-            realignment_loss = get_realignment_loss_for_batch(model, realignment_batch).loss
+                realignment_loss = get_realignment_loss_for_batch(model, realignment_batch).loss
 
-        if torch.cuda.device_count() > 1:
-            outputs = torch.nn.parallel.data_parallel(model, None, module_kwargs=batch)
-            tmp_loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-            task_loss += tmp_loss.mean()
-        else:
-            batch = bring_batch_to_model(batch, model)
-            outputs = model(**batch)
-            task_loss += outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-        accumulated_steps += 1
+        if batch is not None:
+            if torch.cuda.device_count() > 1:
+                outputs = torch.nn.parallel.data_parallel(model, None, module_kwargs=batch)
+                tmp_loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+                task_loss += tmp_loss.mean()
+            else:
+                batch = bring_batch_to_model(batch, model)
+                outputs = model(**batch)
+                task_loss += outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+            accumulated_steps += 1
 
-        if i % task_accumulation_steps == task_accumulation_steps - 1:
-            task_loss /= accumulated_steps
+        if i % task_accumulation_steps == task_accumulation_steps - 1 or i == nb_iter - 1:
+            task_loss /= max(1, accumulated_steps)
 
             # Note that the coefficient is already in the model definition
             total_loss = task_loss + realignment_loss
@@ -78,24 +106,3 @@ def during_strategy_epoch_loop(
                             "task_loss": task_loss,
                         }
                     )
-    if i % task_accumulation_steps != task_accumulation_steps - 1:
-        task_loss /= accumulated_steps
-        total_loss = task_loss + realignment_loss
-
-        total_loss.backward()
-
-        optimizer.step()
-
-        if logging_steps is not None and (i // task_accumulation_steps) % logging_steps == 0:
-            batch_seen = math.ceil(i / task_accumulation_steps)
-
-            logging.info(f"batch: {batch_seen}/{nb_batch} loss : {total_loss}")
-            if log_in_wandb:
-                wandb.log(
-                    {
-                        "train_step": batch_seen,
-                        "train_loss": total_loss,
-                        "realignment_loss": realignment_loss,
-                        "task_loss": task_loss,
-                    }
-                )
--- multilingual_eval/training/training_loops.py
+++ multilingual_eval/training/training_loops.py
@@ -5,14 +5,14 @@ from transformers import DataCollatorForTokenClassification
 from transformers.optimization import AdamW, get_scheduler
 
 
-from multilingual_eval.training.epoch_loop import during_strategy_epoch_loop
+from multilingual_eval.training.epoch_loop import epoch_loop
 from multilingual_eval.datasets.realignment_dataset import (
     RealignmentAndOtherCollator,
 )
 from multilingual_eval.training.evaluation_loops import evaluate_several_token_classification
 
 
-def during_strategy_training_loop(
+def realignment_training_loop(
     tokenizer,
     model,
     task_dataset: DataLoader,
@@ -25,6 +25,7 @@ def during_strategy_training_loop(
     accumulation_steps=1,
     logging_steps=100,
     log_in_wandb=False,
+    strategy="during",
 ):
     if log_in_wandb:
         import wandb
@@ -41,23 +42,39 @@ def during_strategy_training_loop(
         batch_size=task_batch_size,
         collate_fn=DataCollatorForTokenClassification(tokenizer),
     )
-    realignment_dataloader = DataLoader(
-        realignment_dataset,
-        shuffle=False,
-        batch_size=realignment_batch_size,
-        collate_fn=RealignmentAndOtherCollator(
-            tokenizer,
-            DataCollatorForTokenClassification(tokenizer),
-        ),
-    )
+    if strategy != "baseline":
+        realignment_dataloader = DataLoader(
+            realignment_dataset,
+            shuffle=False,
+            batch_size=realignment_batch_size,
+            collate_fn=RealignmentAndOtherCollator(
+                tokenizer,
+                DataCollatorForTokenClassification(tokenizer),
+            ),
+        )
+    else:
+        realignment_dataloader = None
+
+    if strategy == "before":
+        for i in range(n_epochs):
+            epoch_loop(
+                model,
+                optimizer,
+                task_dataloader=None,
+                realignment_dataloader=realignment_dataloader,
+                task_accumulation_steps=accumulation_steps,
+                logging_steps=logging_steps,
+                log_in_wandb=log_in_wandb,
+                nb_iter=len(task_dataloader),
+            )
 
     for i in range(n_epochs):
 
-        during_strategy_epoch_loop(
+        epoch_loop(
             model,
             optimizer,
-            task_dataloader,
-            realignment_dataloader,
+            task_dataloader=task_dataloader,
+            realignment_dataloader=realignment_dataloader if strategy == "during" else None,
             task_accumulation_steps=accumulation_steps,
             logging_steps=logging_steps,
             log_in_wandb=log_in_wandb,
@@ -76,22 +93,28 @@ def during_strategy_training_loop(
             if log_in_wandb:
                 wandb.log(res)
 
+    if strategy == "after":
+        for i in range(n_epochs):
+            epoch_loop(
+                model,
+                optimizer,
+                task_dataloader=None,
+                realignment_dataloader=realignment_dataloader,
+                task_accumulation_steps=accumulation_steps,
+                logging_steps=logging_steps,
+                log_in_wandb=log_in_wandb,
+                nb_iter=len(task_dataloader),
+            )
 
-def before_strategy_training_loop(
-    model, task_dataloader: DataLoader, realignment_dataloader: DataLoader, n_epochs=10
-):
-    pass
-
-
-def after_strategy_training_loop(
-    model, task_dataloader: DataLoader, realignment_dataloader: DataLoader, n_epochs=10
-):
-    pass
-
-
-def realignment_only_training_loop(model, realignment_dataloader: DataLoader, n_epochs=10):
-    pass
-
-
-def task_only_training_loop(model, task_dataloader: DataLoader, n_epochs=10):
-    pass
+            if evaluation_datasets is not None:
+                res = evaluate_several_token_classification(
+                    tokenizer,
+                    model,
+                    evaluation_datasets,
+                    batch_size=task_batch_size,
+                    prefixes=evaluation_prefixes,
+                    overall_prefix="eval",
+                )
+                logging.info(res)
+                if log_in_wandb:
+                    wandb.log(res)
