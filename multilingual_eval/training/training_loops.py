cff8170c7bad53fca6e4d1ccf2fbd2003cfba7ea
Félix Gaschi <flx.gsch@gmail.com>
2022-09-22 09:13:42 +0200

    feat: add same evaluation to new training loop

--- multilingual_eval/training/training_loops.py
+++ multilingual_eval/training/training_loops.py
@@ -11,6 +11,7 @@ from multilingual_eval.datasets.realignment_dataset import (
 )
 from multilingual_eval.training.evaluation_loops import (
     evaluate_several_token_classification,
+    evaluate_token_classification,
 )
 
 
@@ -21,6 +22,7 @@ def realignment_training_loop(
     task_dataset: DataLoader,
     realignment_dataset: DataLoader,
     evaluation_datasets=None,
+    same_language_evaluation_dataset=None,
     evaluation_prefixes=None,
     task_batch_size=4,
     realignment_batch_size=2,
@@ -57,6 +59,14 @@ def realignment_training_loop(
         )
     else:
         realignment_dataloader = None
+
+    if same_language_evaluation_dataset is not None:
+        same_language_evaluation_dataloader = DataLoader(
+            same_language_evaluation_dataset,
+            shuffle=False,
+            batch_size=task_batch_size,
+            collate_fn=DataCollatorForTokenClassification(tokenizer),
+        )
 
     if strategy == "before":
         for i in range(n_epochs):
@@ -95,6 +105,13 @@ def realignment_training_loop(
             logging.info(res)
             if log_in_wandb:
                 wandb.log(res)
+        if same_language_evaluation_dataloader is not None:
+            res = evaluate_token_classification(
+                model, same_language_evaluation_dataloader, prefix="eval_same"
+            )
+            logging.info(res)
+            if log_in_wandb:
+                wandb.log(res)
 
     if strategy == "after":
         for i in range(n_epochs):
@@ -117,6 +134,13 @@ def realignment_training_loop(
                     batch_size=task_batch_size,
                     prefixes=evaluation_prefixes,
                     overall_prefix="eval",
+                )
+                logging.info(res)
+                if log_in_wandb:
+                    wandb.log(res)
+            if same_language_evaluation_dataloader is not None:
+                res = evaluate_token_classification(
+                    model, same_language_evaluation_dataloader, prefix="eval_same"
                 )
                 logging.info(res)
                 if log_in_wandb:
