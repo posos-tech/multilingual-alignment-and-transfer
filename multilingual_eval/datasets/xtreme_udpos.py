9f3924e99605cf1f0744c2457e639f3f7edc7f38
Félix Gaschi <flx.gsch@gmail.com>
2022-10-13 11:48:43 +0200

    fix: use same POS datasets as Wu et al.

--- multilingual_eval/datasets/xtreme_udpos.py
+++ multilingual_eval/datasets/xtreme_udpos.py
@@ -15,6 +15,22 @@ get_xtreme_udpos = get_token_classification_getter(
     "pos_tags",
 )
 
+wuetal_subsets = {
+    "en": "en_ewt",
+    "ar": "ar_padt",
+    "es": "es_gsd",
+    "fr": "fr_gsd",
+    "ru": "ru_gsd",
+    "zh": "zh_gsd",
+}
+
+get_wuetal_udpos = get_token_classification_getter(
+    lambda lang, cache_dir=None: load_dataset(
+        "universal_dependencies", wuetal_subsets[lang], cache_dir=cache_dir
+    ),
+    "upos",
+)
+
 
 def get_xtreme_udpos_langs():
     """
