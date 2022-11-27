1ec6fca3d471fab23000dfa3c2e28363be2f8169
felixgaschi <felix.gaschi@telecom-paristech.fr>
2022-11-27 12:05:56 +0100

    refactor(realignment): improve segmenter install

--- download_resources/stanford_tokenizer.sh
+++ download_resources/stanford_tokenizer.sh
@@ -11,7 +11,12 @@ fi
 
 cd stanford-corenlp-full-2016-10-31
 
-wget http://nlp.stanford.edu/software/stanford-chinese-corenlp-2016-10-31-models.jar
-wget https://raw.githubusercontent.com/stanfordnlp/CoreNLP/master/src/edu/stanford/nlp/pipeline/StanfordCoreNLP-chinese.properties 
+if [ ! -f stanford-chinese-corenlp-2016-10-31-models.jar ]; then
+    wget http://nlp.stanford.edu/software/stanford-chinese-corenlp-2016-10-31-models.jar
+fi
+
+if [ ! -f StanfordCoreNLP-chinese.properties ]; then
+    wget https://raw.githubusercontent.com/stanfordnlp/CoreNLP/master/src/edu/stanford/nlp/pipeline/StanfordCoreNLP-chinese.properties 
+fi
 
 cd ../..
