28e56ca552795c159696b699bfeebe93f6a85b8a
Félix Gaschi <flx.gsch@gmail.com>
2022-12-15 12:37:17 +0100

    fix(realignment): fix awesome-align installation

--- download_resources/awesome_align.sh
+++ download_resources/awesome_align.sh
@@ -15,19 +15,19 @@ cd tools
 
 if [ ! -d awesome-align ]; then
     git clone git@github.com:neulab/awesome-align.git
-    cd awesome-align
-
-    source ../../venvs/awesome-align/bin/activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
-    python setup.py install
-
-    deactivate
-
-    cd ..
+    
 fi
 
-cd ..
+cd awesome-align
+
+source $DATA_DIR/venvs/awesome-align/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python setup.py install
+
+deactivate
+
+cd ../..
 
 if [ ! -d $MODEL_DIR/model_without_co ]; then
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1IcQx6t5qtv4bdcGjjVCwXnRkpr67eisJ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1IcQx6t5qtv4bdcGjjVCwXnRkpr67eisJ" -O $MODEL_DIR/awesome_without_co.zip && rm -rf /tmp/cookies.txt
