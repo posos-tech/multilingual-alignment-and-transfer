0b03302163c0f061dcf58ee5fd304ed3f4fb93d2
Félix Gaschi <flx.gsch@gmail.com>
2023-05-29 16:29:49 +0200

    feat(chore): add imitate_wandb_sweep

--- multilingual_eval/training/wandb_utils.py
+++ multilingual_eval/training/wandb_utils.py
@@ -1,15 +1,25 @@
 import sys
-import wandb
+import csv
 import datasets
 import traceback
-from typing import Callable, Any, Optional
+import itertools
+from typing import Callable, Any, Optional, List
 
-from multilingual_eval.utils import post_on_slack
+from multilingual_eval.loggers import post_on_slack
 from multilingual_eval.tokenization.chinese_segmenter import StanfordSegmenter
 
 
-def wrap_train(train_fn: Callable[[dict, dict, Optional[StanfordSegmenter]],Any], sweep_config: dict, sweep_id: str, zh_segmenter: Optional[StanfordSegmenter]=None, ts=None):
+def wrap_train(
+    train_fn: Callable[[dict, dict, Optional[StanfordSegmenter]], Any],
+    sweep_config: dict,
+    sweep_id: str,
+    zh_segmenter: Optional[StanfordSegmenter] = None,
+    ts=None,
+):
+    import wandb
+
     datasets.disable_progress_bar()
+
     def train(config=None):
         with wandb.init():
             config = wandb.config
@@ -45,3 +55,29 @@ def wrap_train(train_fn: Callable[[dict, dict, Optional[StanfordSegmenter]],Any]
                 raise e
 
     return train
+
+
+def imitate_wandb_sweep(sweep_config):
+    if sweep_config["method"] != "grid":
+        raise NotImplementedError(
+            f"function `imitate_wandb_sweep` was only implemented for method 'grid', got {sweep_config['method']}"
+        )
+
+    fixed_keys = list(sweep_config["parameters"].keys())
+
+    for values in itertools.product(
+        *map(lambda x: sweep_config["parameters"][x]["values"], fixed_keys)
+    ):
+        yield dict(zip(fixed_keys, values))
+
+
+def store_dicts_in_csv(fname: str, infos: List[dict]):
+    if len(infos) == 0:
+        return
+    column_names = set(infos[0].keys()).union(*map(lambda x: x.keys(), infos))
+
+    with open(fname, "w", newline="") as f:
+        writer = csv.writer(f, delimiter=",")
+        writer.writerow(column_names)
+        for info in infos:
+            writer.writerow([str(info.get(c, "")) for c in column_names])
