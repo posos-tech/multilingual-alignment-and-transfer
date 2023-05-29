c0e91e123b649b1144ab9491e52e5c79d00ad645
Félix Gaschi <flx.gsch@gmail.com>
2023-05-29 16:30:29 +0200

    refactor(chore): remove deprecated post_on_slack in utils

--- multilingual_eval/utils.py
+++ multilingual_eval/utils.py
@@ -277,39 +277,6 @@ def get_metric_fn():
     return compute_metrics
 
 
-def post_on_slack(message: str, thread_ts=None):
-    webhook = os.getenv("SLACK_WEBHOOK")
-    channel = os.getenv("SLACK_CHANNEL")
-    slack_oauth = os.getenv("SLACK_OAUTH")
-
-    if webhook is None:
-        return
-
-    if "requests" not in locals():
-        import requests
-
-    if channel is not None and slack_oauth is not None:
-        res = requests.post(
-            "https://slack.com/api/chat.postMessage",
-            json={
-                "channel": channel,
-                "text": message,
-                **({"thread_ts": thread_ts} if thread_ts is not None else {}),
-            },
-            headers={"Authorization": f"Bearer {slack_oauth}"},
-        )
-        try:
-            result = res.json().get("ts")
-        except:
-            logging.error(f"slack alert: could not retrieve `ts`: {res.content}")
-            result = None
-        return result
-
-    requests.post(webhook, json={"text": message})
-
-    return None
-
-
 def count_lines(fname: str):
     """
     Count line in file
