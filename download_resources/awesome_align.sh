31b2630683790730b42b591f46e9bf304cbe0674
Félix Gaschi <flx.gsch@gmail.com>
2023-05-29 16:28:50 +0200

    fix(download): remove env from awesome

--- download_resources/awesome_align.sh
+++ download_resources/awesome_align.sh
@@ -7,10 +7,6 @@ MODEL_DIR=$1
 
 mkdir -p $MODEL_DIR
 
-if [ ! -d $DATA_DIR/venvs/awesome-align ]; then
-    python -m venv $DATA_DIR/venvs/awesome-align
-fi
-
 cd tools
 
 if [ ! -d awesome-align ]; then
@@ -18,16 +14,7 @@ if [ ! -d awesome-align ]; then
     
 fi
 
-cd awesome-align
-
-source $DATA_DIR/venvs/awesome-align/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python setup.py install
-
-deactivate
-
-cd ../..
+cd ..
 
 if [ ! -d $MODEL_DIR/model_without_co ]; then
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1IcQx6t5qtv4bdcGjjVCwXnRkpr67eisJ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1IcQx6t5qtv4bdcGjjVCwXnRkpr67eisJ" -O $MODEL_DIR/awesome_without_co.zip && rm -rf /tmp/cookies.txt
